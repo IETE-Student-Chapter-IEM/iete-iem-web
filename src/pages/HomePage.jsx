ef7986211cdfa763651e6cdb9a1048ebb3cb6e10
Iman Datta <dattaiman56@gmail.com>
2026-09-08 00:12:24 +0530

    Finalise hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,9 @@
 import Hero from "../components/homepage/Hero";
-import HeroTwo from "../components/homepage/HeroTwo";
-import HeroThree from "../components/homepage/HeroThree";
-import HeroFour from "../components/homepage/HeroFour";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
-      <HeroTwo />
-      <HeroThree />
-      <HeroFour />
     </main>
   );
 }
